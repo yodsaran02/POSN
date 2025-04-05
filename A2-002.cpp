c65259482efdfae6777623fabd269e4a83a72121
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-04-05 11:29:38 +0000

    A2-002-1

---
+++ A2-002.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int num_point;
+    int max = 0;
+    cin >> num_point;
+    int points[num_point][2];
+    for(int i = 0; i < num_point; i++){
+        cin >> points[i][0];
+        cin >> points[i][1];
+    }
+    for(int i = 0; i < num_point; i++){
+        //cout << i;
+        for(int j = i + 1; j < num_point; j++){
+            //cout << j;
+            if((points[i][0] - points[j][0]) == (points[i][1] - points[j][1])){
+                max = (abs(points[i][0] - points[j][0]) > max) ? abs(points[i][0] - points[j][0]) : max;
+                //cout << "Found!";
+            }
+        }
+    }
+    cout << max;
+}
