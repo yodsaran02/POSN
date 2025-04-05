aa75174ec78c4218ebe96c5759d6e1b7195ff072
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-04-05 10:21:33 +0000

    1

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(){
-    string shit;
+    string first,last;
+    cin >> first;
+    cin >> last;
+    cout << "Hello " << first << " " << last << "\n";
+    cout << first[0] << first[1] << last[0] << last[1];
 }
