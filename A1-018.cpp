329b42d459db53b94f658e016859a01b15ac3c76
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-04-20 14:19:46 +0000

    A1-018

---
+++ A1-018.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+int main(){
+    string r_number[9] = {"I","II","III","IV","V","VI","VII","VIII","IX"};
+    int number;
+    cin >> number;
+    if(number <= 9 && number >= 1){
+        cout << r_number[number-1];
+    }else{
+        cout << "Error : Out of range";
+    }
+}
