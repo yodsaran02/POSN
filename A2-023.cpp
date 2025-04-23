0537ed9432f026f62b573bdf4006b7bc59addf14
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-04-23 10:32:26 +0000

    A2-023-4

--- A2-023.cpp
+++ A2-023.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 int main(){
-    string msg, msg_lower;
+    string msg;
     int isValid = 0;
     int maxA = 0;
     int countA = 0;
     cin >> msg;
-    transform(msg.begin(), msg.end(), msg_lower.begin(), [](char c){
+    transform(msg.begin(), msg.end(), msg.begin(), [](char c){
         return tolower(c);
     });
     for(int i = 0; i < msg.length(); i++){
-        if(msg_lower[i] == 'r'){
-            for(int j = 1; j < msg.length()-(i+1); j++){
-                if(msg_lower[i+j] == 'a'){
+        if(msg[i] == 'r'){
+            for(int j = 1; j < msg.length()-i; j++){
+                if(msg[i+j] == 'a'){
                     countA++;
                     isValid++;
                 }else if(countA == 0){ 
@@ -29,13 +29,20 @@ int main(){
             //cout << countA << endl;
             i += countA;
             countA = 0;
-        }else if(msg_lower[i] == 'a' && msg_lower[i-1] != 'r'){
-            cout << "no " << (i);
-            exit(0);
-        }else if(msg_lower[i] == 'b' && (msg_lower[i+1] != 'i' && msg_lower[i+1] != 't')){
-            cout << "no " << (i+1);
-            exit(0);
-        }else if(msg_lower[i] == 'b'){ isValid++; }
+        }else if(msg[i] == 'a' ){
+            if (i == 0 || msg[i-1] != 'r') {
+                cout << "no " << i;
+                exit(0);
+            }
+        }else if(msg[i] == 'b' && i != (msg.length()-1)){
+            if(msg[i+1] != 'i' && msg[i+1] != 't'){
+                cout << "no " << (i+1);
+                exit(0);
+            }else{
+                i++;
+                isValid++;
+            }
+        }else if(msg[i] == 'b'){ isValid++; }
     }
     if(isValid){ cout << "yes " << maxA << endl; }
     if(!isValid){ cout << "unknown " << msg.length(); }
