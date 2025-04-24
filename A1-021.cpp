e9463bae1c4131f38747afa91ef16a3fcfe7d86d
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-04-24 14:17:17 +0000

    A1-021

---
+++ A1-021.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int year;
+    cin >> year;
+    if(year == 1500){
+        cout << "yes";
+    }else if(year % 400 == 0){
+        cout << "yes";
+    }else if(year % 100 == 0){
+        cout << "no";
+    }else if(year % 4 == 0){
+        cout << "yes";
+    } 
+}
