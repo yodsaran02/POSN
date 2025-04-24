fbad31a8f6bfed1126f336f86f79f9d7783234ec
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-04-24 14:18:51 +0000

    A1-021-2

--- A1-021.cpp
+++ A1-021.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int year;
     cin >> year;
-    if(year == 1500){
+    if(year <= 1582 && year % 4 == 0){
         cout << "yes";
     }else if(year % 400 == 0){
         cout << "yes";
