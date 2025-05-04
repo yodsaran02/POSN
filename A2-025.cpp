bb7686141241a31fdc177dd250951eccde7fc058
Yodsaran Panomratanarak <yodsaran02@gmail.com>
2025-05-04 18:23:05 +0000

    A2-025-doing

---
+++ A2-025.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int numc, numr, posc, pocr, n;
+	cin >> numr;
+	cin >> numc;
+	int empty = numr * numc;
+	vector<vector<int>> grid(numr, vector<int>(numc, 0));
+	cin >> posr;
+	cin >> posc;
+	cin >> n;
+	int tempr, tempc;
+	for(int i = 0; i < n; i++){
+		cin >> tempr;
+		cin >> tempc;
+		grid[tempr][tempc] = 100;
+
+	}
+}
