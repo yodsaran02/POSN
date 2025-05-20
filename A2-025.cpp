717a56a035a88a541b4d23e4d422efab85e243fd
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-05-20 12:06:39 +0000

    A2-025.cpp

--- A2-025.cpp
+++ A2-025.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 
 int main(){
-	int numc, numr, posc, pocr, n;
+	int numc, numr, posc, posr, n;
 	cin >> numr;
 	cin >> numc;
 	int empty = numr * numc;
@@ -16,7 +17,34 @@ int main(){
 	for(int i = 0; i < n; i++){
 		cin >> tempr;
 		cin >> tempc;
-		grid[tempr][tempc] = 100;
-
+		if(!grid[tempr][tempc]){ empty--; }
+		grid[tempr][tempc] = 100;		
+		for(int j = -2; j <= 2; j++){
+			for(int k = -2; k <= 2; k++){
+				if(tempr+j < 0 || tempc+k < 0 || tempr+j >= numr || tempc+k >= numc){
+					continue;
+				}else if(j == 0 && k == 0){
+					continue;
+				}
+				
+				if(abs(j) == 1 || abs(k) == 1){
+					if(grid[tempr+j][tempc+k] == 100){
+						continue;
+					}else{
+						if(!grid[tempr+j][tempc+k]){ empty--; }
+						grid[tempr+j][tempc+k] = 60;
+					}
+				}else if(abs(j) == 2 || abs(k) == 2){
+					if(grid[tempr+j][tempc+k]){
+						continue;
+					}else{
+						grid[tempr+j][tempc+k] = 20;
+						empty--;
+					}
+				}
+			}
+		}
 	}
+	cout << empty << endl;
+	cout << grid[posr][posc] << "%";
 }
