704b275868c8876cafba18a52e605821f881512f
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-05-22 08:24:39 +0000

    A1-023-2

--- A1-023.cpp
+++ A1-023.cpp
@@ -10,18 +10,18 @@ int main(){
     if(cf == 'c'){
         if(t <= 0){ 
             cout << "solid"; 
-        }else if(0 < t < 100){ 
-            cout << "liquid"; 
         }else if(100 <= t){ 
             cout << "gas"; 
+        }else if(0 < t < 100){ 
+            cout << "liquid"; 
         }
     }else if(cf == 'f'){
         if(t <= 32){ 
             cout << "solid"; 
-        }else if(32 < t < 212){ 
-            cout << "liquid"; 
         }else if(212 <= t){ 
             cout << "gas"; 
+        }else if(32 < t < 212){ 
+            cout << "liquid"; 
         }
     }
 }
